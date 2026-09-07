4ad841f811bfb5d96a7cb3e37f844841c17827eb
emerss001 <emersonnevess80@gmail.com>
2026-09-06 22:16:21 -0300

    fix: adjust page layout to allow vertical scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
             baseTheme: dark,
           }}
         >
-          <div className="flex h-full flex-col overflow-hidden">{children}</div>
+          <div className="flex min-h-full flex-col">{children}</div>
         </ClerkProvider>
       </body>
     </html>
